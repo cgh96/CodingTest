f359b43c80afe4c32e0d1b323c4be8e568e7c8ab
CGH <tt878429@gmail.com>
2022-04-26 12:03:58 +0900

    --

---
+++ baek1316/baek1316.js
@@ -0,0 +1,30 @@
+"use strict";
+const fs = require('fs');
+const input = fs.readFileSync('./baek1316/example.txt').toString().trim().split('\n');
+
+const count = Number(input[0]);
+let wordCounter = 0;
+
+for(let i = 1; i<=count; i++){
+    const temp = input[i];
+    const arr = [];
+    let check=true;
+
+    for(let j = 0; j<temp.length; j++){
+
+        if(arr.indexOf(temp[j]) === -1){ //지정된 요소를 찾을 수 있는 첫 번째 인덱스 반환 
+            arr.push(temp[j]);           ////or -1반환 (indexOf)
+        }
+        else{
+            if(arr.indexOf(temp[j]) ==! arr.length-1){
+                check = false;
+                break;
+            }
+        }
+    }
+    if(check === true){
+        wordCounter++;
+    }
+}
+
+console.log(wordCounter);
