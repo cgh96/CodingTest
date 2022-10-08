ab696f2e7f3b715100924f228cc4873a6f3be0ee
CGH <tt878429@gmail.com>
2022-10-09 00:32:32 +0900

    10/09 friends4Block solved

--- pg_프렌즈4블록/friends4blocks.js
+++ pg_프렌즈4블록/friends4blocks.js
@@ -1,44 +1,71 @@
 function solution(m, n, board) {
-    let changeBoard = new Array(n).fill().map(elem => elem = []);
+    let RotateBoard = new Array(n).fill().map(elem => elem = []);
     let check = new Array(n).fill().map(elem => elem = []);
-    let count = 0;
-    let tmp = 0;
-    for(let c of check) {
-        for(let i = 0; i < m; i++) {
-            c.push(false);
-        }
-    }
-    
+    let removeCount = 0;
     board = board.map(elem => elem.split(""));
 
-    for(let i = n - 1; i >= 0; i--) {
+    for(let c of check) {
+        for(let i = 0; i < m; i++) { c.push(false); }
+    }
+
+    for(let i = 0; i < n; i++) {
         for(let j = m - 1; j >= 0; j--) {
-            changeBoard[i].push(board[j][i]);
+            RotateBoard[i].push(board[j][i]);
         }
     }
-    
     while(true) {
-        for(let i = 0; i < changeBoard.length - 1; i++) {
-            for(let j = 0; j < changeBoard[i].length - 1; j++) {
-                if(changeBoard[i][j] === changeBoard[i][j+1] 
-                    && changeBoard[i+1][j] === changeBoard[i+1][j+1]
-                    && changeBoard[i][j+1] === changeBoard[i+1][j]) {
+        for(let i = 0; i < RotateBoard.length - 1; i++) {
+            for(let j = 0; j < RotateBoard[i].length - 1; j++) {
+                if(RotateBoard[i][j] === RotateBoard[i][j+1] &&
+                   RotateBoard[i][j] === RotateBoard[i+1][j+1] &&
+                   RotateBoard[i][j] === RotateBoard[i+1][j]) {
                     check[i][j] = true;
                     check[i][j+1] = true;
-                    check[i+1][j] = true;
                     check[i+1][j+1] = true;
+                    check[i+1][j] = true;
                 }
             }
         }
-
-        for(let i = 0; i < n; i++) {
-            let trueNum = check[i].filter(el => el === true).length;
-            tmp += trueNum;
-            count += trueNum;
-            changeBoard[i].splice(check[i].findIndex(el => el === true), trueNum);
-            check[i] = check[i].map(elem => elem = false);
+        let tmpCount = 0;
+        for(let i = 0; i < RotateBoard.length; i++) {
+            let trueArray = [];
+            trueArray = FindtrueIndex(check[i]);
+            tmpCount += trueArray.length;
+            removeCount += trueArray.length;
+            removeTrueElem([...trueArray], RotateBoard[i]);
+            removeTrueElem([...trueArray], check[i]);
         }
-        if(tmp === 0) { return count; }
-        tmp = 0;
+        if(tmpCount === 0) { console.log(removeCount); return removeCount; }
     }
 }
+
+function FindtrueIndex (check) {
+    let trueIndexArray = [];
+    check.forEach( (el, idx) => {
+        if(el) {
+            trueIndexArray.push(idx);
+        }
+    })
+    return [...trueIndexArray];
+}
+
+function removeTrueElem (trueArr, RotateBoard) {
+    while(trueArr.length > 0) {
+        let index = trueArr.pop();
+        RotateBoard.splice(index, 1);
+    }
+    return [...RotateBoard];
+}
+
+solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]);
+
+
+/**
+ * 0 1 2 3 4 5
+ 0 T T T A N T
+ 1 R R F A C C
+ 2 R R R F C C
+ 3 T R R R A A
+ 4 T T M M M F
+ 5 T M M T T J
+ */
