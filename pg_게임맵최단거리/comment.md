c5b07d564cd6d1474413a820752bfccc5a6f51d7
CGH <tt878429@gmail.com>
2022-10-18 00:16:20 +0900

    10/18

--- pg_게임맵최단거리/comment.md
+++ pg_게임맵최단거리/comment.md
@@ -51,3 +51,51 @@ function solution(maps) {
 ```
 
 ## BFS로 풀어야 할 것 같다.
+```
+function solution(maps) {
+    let queue = [];
+    const ROW_END = maps.length - 1;
+    const COL_END = maps[0].length - 1;
+    let col = 0;
+    let row = 0;
+    let cost = 1;
+
+    queue.push([row, col, cost]);
+    maps[0][0] = 0;
+
+    while(queue.length !== 0) {
+        row = queue[0][0];
+        col = queue[0][1];
+        cost = queue[0][2];
+        queue.splice(0, 1);
+
+        if(row === ROW_END && col === COL_END) return cost;
+
+        if(row < ROW_END) {
+            if(maps[row + 1][col]) { 
+                queue.push([row + 1, col, 1 + cost]);
+                maps[row + 1][col] = 0;
+            }
+        }
+        if(col < COL_END) {
+            if(maps[row][col + 1]) { 
+                queue.push([row, col + 1, 1 + cost]);
+                maps[row][col + 1] = 0;
+            }
+        }
+        if(row > 0) {
+            if(maps[row - 1][col]) { 
+                queue.push([row - 1, col, 1 + cost]); 
+                maps[row - 1][col] = 0;
+            }
+        }
+        if(col > 0) {
+            if(maps[row][col - 1]) {
+                queue.push([row, col - 1, 1 + cost]); 
+                maps[row][col - 1] = 0;
+            }
+        }
+    }
+    return -1;
+}
+```
--- pg_게임맵최단거리/gameMapShort.js
+++ pg_게임맵최단거리/gameMapShort.js
@@ -1,58 +1,48 @@
 function solution(maps) {
-    let dist = 25;
-    let distMaps = new Array(maps.length).fill().map(elem => new Array(maps[0].length).fill(0) );
-    distMaps[0][0] = 1;
+    let queue = [];
     const ROW_END = maps.length - 1;
     const COL_END = maps[0].length - 1;
+    let col = 0;
+    let row = 0;
+    let cost = 1;
 
+    queue.push([row, col, cost]);
+    maps[0][0] = 0;
 
-    function DFS(row, col, count) {
-        if(row === ROW_END && col === COL_END) {
-            dist = count < dist ? count : dist;
-        }
+    while(queue.length !== 0) {
+        row = queue[0][0];
+        col = queue[0][1];
+        cost = queue[0][2];
+        queue.splice(0, 1);
+
+        if(row === ROW_END && col === COL_END) return cost;
 
         if(row < ROW_END) {
-            if(maps[row + 1][col] && (distMaps[row + 1][col] === 0 || distMaps[row + 1][col] > count + 1)) { 
-                distMaps[row + 1][col] = count + 1;
-                DFS(row + 1, col, count + 1);
+            if(maps[row + 1][col]) { 
+                queue.push([row + 1, col, 1 + cost]);
+                maps[row + 1][col] = 0;
             }
         }
-        
         if(col < COL_END) {
-            if(maps[row][col + 1] && (distMaps[row][col + 1] === 0 || distMaps[row][col + 1] > count + 1)) { 
-                distMaps[row][col + 1] = count + 1;
-                DFS(row, col + 1, count + 1); 
+            if(maps[row][col + 1]) { 
+                queue.push([row, col + 1, 1 + cost]);
+                maps[row][col + 1] = 0;
             }
         }
-       
         if(row > 0) {
-            if(maps[row - 1][col] && (distMaps[row - 1][col] === 0 || distMaps[row - 1][col] > count + 1)) { 
-                distMaps[row - 1][col] = count + 1;
-                DFS(row - 1, col, count + 1); 
-            } 
+            if(maps[row - 1][col]) { 
+                queue.push([row - 1, col, 1 + cost]); 
+                maps[row - 1][col] = 0;
+            }
         }
         if(col > 0) {
-            if(maps[row][col - 1] && (distMaps[row][col - 1] === 0 || distMaps[row][col - 1] > count + 1)) { 
-                distMaps[row][col - 1] = count + 1;
-                DFS(row, col - 1, count + 1); 
+            if(maps[row][col - 1]) {
+                queue.push([row, col - 1, 1 + cost]); 
+                maps[row][col - 1] = 0;
             }
         }
-        
     }
-
-    DFS(0, 0, 1);
-    return dist === 25 ? -1 : dist;
+    return -1;
 }
 
-solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,1,0],[0,0,0,1,1]]);
-
-
-/**  0 1 2 3 4 
- *       
-0    1 0 1 1 1
-1    1 0 1 0 1
-2    1 0 1 1 1
-3    1 1 1 0 1
-4    0 0 0 0 1
-
- */
+solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]);
