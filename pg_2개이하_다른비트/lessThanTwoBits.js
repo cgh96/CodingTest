b369adcce059724c61bddc82370bef12f97513af
CGH <tt878429@gmail.com>
2022-12-13 18:07:06 +0900

    12/13

---
+++ pg_2개이하_다른비트/lessThanTwoBits.js
@@ -0,0 +1,29 @@
+function solution(numbers) {
+    numbers = numbers.map(num => {
+        let str = ("0" + num.toString(2)).split("");
+
+        let zeroIndex = str.lastIndexOf("0");
+        
+        if(zeroIndex === str.length - 1) {
+            str[str.length - 1] = "1";     
+        } else {
+            str[zeroIndex] = "1";
+            str[zeroIndex + 1] = "0";
+        }
+        return parseInt(str.join(""), 2);
+    })
+    return numbers;
+}
+
+solution([2,7]);
+
+// x < numbers
+
+/**
+ * 1000
+ * 0100
+ * 10111
+ * 01
+ * 11011
+ * 
+ */
