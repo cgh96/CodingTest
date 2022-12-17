cdb9c36640fdb3c2857dbccbbf08d2e3688a7f20
CGH <tt878429@gmail.com>
2022-12-17 16:39:46 +0900

    12/17

---
+++ pg_124나라/124country.js
@@ -0,0 +1,39 @@
+function solution(n) {
+    let rest = 0;
+    let answer = "";
+   
+    while( n >= 3) {
+        rest = n % 3;
+        n = Math.floor(n / 3);
+
+        if(rest === 0) {
+            answer = "4" + answer;
+            n = n - 1;
+        } else {
+            answer = rest.toString() + answer;
+        }
+    }
+
+    if(n > 0) {
+        answer = n.toString() + answer;
+    }
+    return answer;
+}
+
+solution(10);
+
+/**
+15 / 3 = 5...0
+4 /  3 = 1...1
+
+13 / 3 = 4...1
+4 / 3 = 1...1
+
+10 / 3 = 3...1
+3 / 3 = 1...0
+
+3 / 3 = 1...0
+
+ 3의 배수 => (c-1)4
+ 
+ */
