31d471b0e9d9bd8b85f16d89b9438c1d79432f87
cgh96 <tt878429@gmail.com>
2024-02-24 13:30:01 +0900

    백준 16948 실버1 데스 나이트 BFS

--- 스터디/스터디_2주차/baek16948_데스_나이트/deathKnight.js
+++ 스터디/스터디_2주차/baek16948_데스_나이트/deathKnight.js
@@ -2,13 +2,54 @@
 const fs = require("fs");
 const filePath =
   process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n")
-  .map((e) => +e);
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const TABLE_SIZE = +input[0];
+console.log(BFS());
 
-console.log(input);
+function BFS() {
+  const TABLE_SIZE = +input[0];
+  const [startX, startY, endX, endY] = input[1].split(" ").map((e) => +e);
+  const VISITED = new Array(200).fill().map((_) => new Array(200).fill(false));
+
+  const dir = [
+    [-2, -1],
+    [-2, 1],
+    [0, -2],
+    [0, 2],
+    [2, -1],
+    [2, 1],
+  ];
+
+  const queue = [[startX, startY, 0]];
+  VISITED[startX][startY] = true;
+
+  while (queue.length > 0) {
+    const [curX, curY, curMoveCnt] = queue.shift();
+
+    if (curX === endX && curY === endY) {
+      return curMoveCnt;
+    }
+
+    for (let i = 0; i < 6; i++) {
+      const [nextX, nextY, nextMoveCnt] = [
+        curX + dir[i][0],
+        curY + dir[i][1],
+        curMoveCnt + 1,
+      ];
+
+      if (
+        nextX < 0 ||
+        nextX >= TABLE_SIZE ||
+        nextY < 0 ||
+        nextY >= TABLE_SIZE ||
+        VISITED[nextX][nextY]
+      ) {
+        continue;
+      }
+      queue.push([nextX, nextY, nextMoveCnt]);
+      VISITED[nextX][nextY] = true;
+    }
+  }
+
+  return -1;
+}
